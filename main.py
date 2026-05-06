45da7450b37bda2525b0eab86fc7a9697562e1b1
sakshiselmokar <selmokarsakshi0@gmail.com>
2026-05-07 01:20:44 +0530

    switch to lighter model

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ app.add_middleware(
 )
 
 HF_TOKEN = os.environ.get("HF_TOKEN", "")
-HF_URL = "https://api-inference.huggingface.co/models/typeform/distilbert-base-uncased-mnli"LABELS   = [
+HF_URL = "https://api-inference.huggingface.co/models/typeform/distilbert-base-uncased-mnli"
+LABELS   = [
     "Deep Work",
     "Procrastination",
     "Debugging Panic",
